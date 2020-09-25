bcb2107ce1adb7db75d0cd1f280219dd01d7e741
user-code-web <54837128+user-code-web@users.noreply.github.com>
2020-09-26 03:13:31 +0530

    added links

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 var containAll = document.getElementsByClassName("contain-all")[0];
 var headerNavBox = document.getElementsByClassName("header-navbar-box")[1];
+var headerNavBoxLogo = document.getElementsByClassName("header-navbar-box")[0];
+var headerNavbar = headerNavBoxLogo.parentElement;
 var headerIcon = document.getElementsByClassName("header-icon")[0];
 var containAllOverlay = document.getElementsByClassName("contain-all-overlay")[0];
+var currScroll = window.pageYOffset, prevScroll;
 function headerToggle(ele) {
     ele.setAttribute("onclick", null);
     setTimeout((function(){ele.setAttribute("onclick", "headerToggle(this)")}), 500);
@@ -11,8 +14,12 @@ function headerToggle(ele) {
         containAll.style.marginRight = "0px";
         headerNavBox.style.width = "0px";
         setTimeout(function(){headerNavBox.style.visibility = "hidden";}, 500);
+        containAll.children[0].style.width = "0%";
+        containAll.children[0].style.opcaity = "0.1";
     }
     else {
+        containAll.children[0].style.width = "100%";
+        containAll.children[0].style.opacity = "0.8";
         headerNavBox.style.visibility = "visible";
         headerNavBox.style.width = "250px";
         containAll.style.marginRight = "250px";
@@ -31,6 +38,45 @@ window.addEventListener('resize', function(){
         if (containAll.style.marginRight == "0px" || containAll.style.marginRight == ""){
             headerNavBox.style.width = "0px";
             setTimeout(function(){headerNavBox.style.visibility = "hidden";}, 500);
+            containAll.children[0].style.width = "0%";
+            containAll.children[0].style.opcaity = "0.1";
         }
     }
 });
+
+window.onscroll = function(){
+    prevScroll = currScroll;
+    currScroll = window.pageYOffset;
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30){
+        if (prevScroll < currScroll){
+            headerNavbar.style.top = "-80px";
+        }
+        else{
+            headerNavbar.style.top = "0px";
+        }
+        headerNavbar.style.minHeight="80px";
+        headerNavbar.style.backgroundColor="white";
+        headerNavbar.style.boxShadow="1px 1px 5px 2px grey";
+    }
+    else{
+        headerNavbar.style.minHeight="60px";
+        headerNavbar.style.backgroundColor="transparent";
+        headerNavbar.style.boxShadow="none";
+    }
+}
+
+window.onmousemove = function(){
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30){
+        if(window.event.clientY <= 120){
+            headerNavbar.style.top = "0px";
+        }
+        else{
+            if (prevScroll < currScroll){
+                headerNavbar.style.top = "-80px";
+            }
+        }
+    }
+    else{
+        headerNavbar.style.top = "0px";
+    }
+}
